b12e47c602f4daa3b5d6396b0ee8b96ebfcc5b92
j4stylez <muriithijames345@gmail.com>
2025-07-27 23:08:40 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,29 +1,21 @@
+// src/components/recipeStore.js
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  favorites: [],
-  recommendations: [],
-
   addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe]
+    recipes: [...state.recipes, recipe],
   })),
-
-  addFavorite: (recipeId) => set((state) => ({
-    favorites: [...new Set([...state.favorites, recipeId])]
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
   })),
-
-  removeFavorite: (recipeId) => set((state) => ({
-    favorites: state.favorites.filter(id => id !== recipeId)
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
   })),
-
-  generateRecommendations: () => set((state) => {
-    const recommended = state.recipes.filter(recipe =>
-      state.favorites.includes(recipe.id) && Math.random() > 0.5
-    );
-    return { recommendations: recommended };
-  }),
 }));
+
 
 
 
