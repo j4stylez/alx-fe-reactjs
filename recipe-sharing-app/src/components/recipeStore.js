c3c4ad4dccc54acb3e0a643e7f2ba595f6acdf34
j4stylez <muriithijames345@gmail.com>
2025-07-27 21:48:56 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,20 @@
-// src/components/recipeStore.js
 import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe],
-  })),
-  deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== id),
-  })),
-  updateRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((recipe) =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    ),
-  })),
+  searchTerm: '',
+  filteredRecipes: [],
+  
+  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes }),
+  
+  setSearchTerm: (term) => {
+    set((state) => {
+      const filtered = state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      );
+      return { searchTerm: term, filteredRecipes: filtered };
+    });
+  },
 }));
+
 
