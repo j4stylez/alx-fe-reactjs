234f7ae6f04625f9c17461eac1ae3c8435aa1ed8
j4stylez <muriithijames345@gmail.com>
2025-07-27 23:36:57 +0300

    Update UserProfile.jsx

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-const UserProfile = (props) => {
+function UserProfile(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{props.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span>
+      </p>
+      <p>
+        Bio: <span>{props.bio}</span>
+      </p>
     </div>
   );
-};
+}
 
 export default UserProfile;
-=======
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
-};
 
-export default UserProfile;
->>>>>>> 5271e74e5bbdf0c198cdfbe6122016784d1cbb0b
