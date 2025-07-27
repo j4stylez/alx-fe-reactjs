8e6fc027d71e7fd800e1cba98b59d047e8ee8e62
j4stylez <muriithijames345@gmail.com>
2025-07-27 23:15:57 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,12 @@
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+    <div>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
-      <FavoritesList />
-      <RecommendationsList />
     </div>
   );
 }
