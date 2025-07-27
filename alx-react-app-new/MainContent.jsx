2cfb454481d6d0fa3be728c8a25c46ef7fca4423
Ernest Njiru <jamesernest456@gmail.com>
2025-07-27 18:09:01 +0300

    Styled Header, MainContent, UserProfile, Footer with inline CSS

--- alx-react-app-new/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-  return (
-    <main style={{ backgroundColor: '#f0f8ff', padding: '15px', fontStyle: 'italic' }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-
-export default MainContent;
