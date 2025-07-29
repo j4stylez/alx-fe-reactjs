55950fdbd3ec78e9733b070eef45cc6dc053c183
j4stylez <muriithijames345@gmail.com>
2025-07-30 02:25:27 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,16 +3,31 @@ import axios from 'axios';
 export async function fetchUserData(username, token) {
   try {
     const headers = token ? { Authorization: `token ${token}` } : {};
-
-    // Use GitHub Search Users API endpoint returning multiple users
     const url = `https://api.github.com/search/users?q=${encodeURIComponent(username)}`;
-
     const response = await axios.get(url, { headers });
-
-    return response.data.items; // array of user objects
+    return response.data.items;
   } catch (error) {
     throw error;
   }
 }
+
+export async function advancedUserSearch({ username, location, minRepos }, token) {
+  try {
+    let query = '';
+
+    if (username) query += `${username} in:login`;
+    if (location) query += ` location:${location}`;
+    if (minRepos) query += ` repos:>=${minRepos}`;
+
+    const headers = token ? { Authorization: `token ${token}` } : {};
+
+    const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+    const response = await axios.get(url, { headers });
+    return response.data.items;
+  } catch (error) {
+    throw error;
+  }
+}
+
 
 
