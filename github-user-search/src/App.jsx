2f25eab4497df63370cb6997581a1e166e036d24
j4stylez <muriithijames345@gmail.com>
2025-07-30 00:49:20 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
       <h1>GitHub User Search</h1>
-      {/* Future components will go here */}
+      <Search />
     </div>
   );
 }
