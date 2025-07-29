8edc808ebcfcc6ba74d1c72404d1133589a646ca
j4stylez <muriithijames345@gmail.com>
2025-07-30 02:22:49 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,23 +1,18 @@
 import axios from 'axios';
 
-export async function advancedUserSearch({ username, location, minRepos }, token) {
+export async function fetchUserData(username, token) {
   try {
-    let query = '';
-
-    if (username) query += `${username} in:login`;
-    if (location) query += ` location:${location}`;
-    if (minRepos) query += ` repos:>=${minRepos}`;
-
     const headers = token ? { Authorization: `token ${token}` } : {};
 
-    // 🔥 ALX checker wants this literal string in the file
-    const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+    // Use GitHub Search Users API endpoint returning multiple users
+    const url = `https://api.github.com/search/users?q=${encodeURIComponent(username)}`;
 
     const response = await axios.get(url, { headers });
 
-    return response.data.items;
+    return response.data.items; // array of user objects
   } catch (error) {
     throw error;
   }
 }
+
 
