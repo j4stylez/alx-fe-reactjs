30083bfce5b62d769c46f3df9555b80bc36db352
j4stylez <muriithijames345@gmail.com>
2025-07-30 00:58:07 +0300

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,12 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export async function fetchUser(username, token) {
+export async function fetchUserData(username, token) {
   try {
-    const headers = token
-      ? { Authorization: `token ${token}` }
-      : {};
-
+    const headers = token ? { Authorization: `token ${token}` } : {};
     const response = await axios.get(`${BASE_URL}/users/${username}`, { headers });
     return response.data;
   } catch (error) {
